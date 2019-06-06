62a91613506c9c8959349fe9e0313c9ec441c0e1
Tommy Sadiq Hinrichsen <th@mustache.dk>
2019-06-06 09:51:10 +0200

    send local notification when updating userdefaults objects

--- MustacheFoundation/Classes/Extensions/UserDefaults+Extensions.swift
+++ MustacheFoundation/Classes/Extensions/UserDefaults+Extensions.swift
@@ -5,9 +5,9 @@ public extension UserDefaults {
     func archive<T: NSCoding>(object: T?, forKey key: String) {
         if let object = object {
             let archive = NSKeyedArchiver.archivedData(withRootObject: object) as NSData
-            UserDefaults.standard.set(archive, forKey: key)
+            self.set(archive, forKey: key)
         } else {
-            UserDefaults.standard.removeObject(forKey: key)
+            self.removeObject(forKey: key)
         }
     }
 
@@ -21,31 +21,36 @@ public extension UserDefaults {
 
     func archive<T: NSCoding>(object: [T], forKey key: String) {
         let archive = NSKeyedArchiver.archivedData(withRootObject: object as NSArray) as NSData
-        UserDefaults.standard.set(archive, forKey: key)
+        self.set(archive, forKey: key)
     }
 
     func unarchive<T: NSCoding>(forKey key: String) -> [T] {
-        if let unarchivedObject = UserDefaults.standard.data(forKey: key) {
+        if let unarchivedObject = self.data(forKey: key) {
             return NSKeyedUnarchiver.unarchiveObject(with: unarchivedObject) as? [T] ?? []
         } else {
             return []
         }
     }
 
-    func decodeObject<T>(forKey defaultName: String) -> T? where T: Decodable {
-        guard let saved = self.object(forKey: defaultName) as? Data else { return nil }
+    func decodeObject<T>(forKey key: String) -> T? where T: Decodable {
+        guard let saved = self.data(forKey: key) as? Data else { return nil }
         let decoder = JSONDecoder()
         let loaded = try? decoder.decode(T.self, from: saved)
         return loaded
     }
 
-    func encode<T>(_ value: T?, forKey defaultName: String) where T: Codable {
+    func encode<T>(_ value: T?, forKey key: String) where T: Encodable {
         let encoder = JSONEncoder()
         guard let value = value, let encoded = try? encoder.encode(value) else {
-            self.removeObject(forKey: defaultName)
+            self.removeObject(forKey: key)
             return
         }
-        self.set(encoded, forKey: defaultName)
+        self.set(encoded, forKey: key)
+
+        let string = "\(UserDefaults.didChangeNotification.rawValue)-\(key)"
+        let name = NSNotification.Name(rawValue: string)
+
+        NotificationCenter.default.post(name: name, object: nil)
     }
 
     func hasValue(forKey key: String) -> Bool {
