b8f8294e00743e12e1ca4f3175a5b97cb1a2604e
Tommy Sadiq Hinrichsen <th@mustache.dk>
2019-09-24 10:16:31 +0200

    Removed date support for stridable

--- MustacheFoundation/Classes/Extensions/Date+Extensions.swift
+++ MustacheFoundation/Classes/Extensions/Date+Extensions.swift
@@ -35,13 +35,13 @@ public extension Date {
     }
 }
 
-extension Date: Strideable {
-
-    public func advanced(by n: Int) -> Date {
-        return Calendar.current.date(byAdding: .day, value: n, to: self) ?? self
-    }
-
-    public func distance(to other: Date) -> Int {
-        return Calendar.current.dateComponents([.day], from: other, to: self).day ?? 0
-    }
-}
+// extension Date: Strideable {
+//
+//     public func advanced(by n: Int) -> Date {
+//         return Calendar.current.date(byAdding: .day, value: n, to: self) ?? self
+//     }
+// 
+//     public func distance(to other: Date) -> Int {
+//         return Calendar.current.dateComponents([.day], from: other, to: self).day ?? 0
+//     }
+// }
