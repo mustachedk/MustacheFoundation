87a7bce8b4d219de59588b7f6e19c41dd1eabc0f
Mustache Developer <developer@mustache.dk>
2019-11-24 12:17:15 +0100

    Update UserDefaults+Extensions.swift
    Notifications not triggered on removal of objects

--- MustacheFoundation/Classes/Extensions/UserDefaults+Extensions.swift
+++ MustacheFoundation/Classes/Extensions/UserDefaults+Extensions.swift
@@ -40,16 +40,17 @@ public extension UserDefaults {
     }
 
     func encode<T>(_ value: T?, forKey key: String) where T: Encodable {
+        
+        let string = "\(UserDefaults.didChangeNotification.rawValue)-\(key)"
+        let name = NSNotification.Name(rawValue: string)
+        
         let encoder = JSONEncoder()
         guard let value = value, let encoded = try? encoder.encode(value) else {
             self.removeObject(forKey: key)
+            NotificationCenter.default.post(name: name, object: nil)
             return
         }
         self.set(encoded, forKey: key)
-
-        let string = "\(UserDefaults.didChangeNotification.rawValue)-\(key)"
-        let name = NSNotification.Name(rawValue: string)
-
         NotificationCenter.default.post(name: name, object: nil)
     }
 
